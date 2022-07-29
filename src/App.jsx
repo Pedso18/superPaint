dd9777987eb47036efa8569f847aaeff930d8b2e
Pedso1812@gmail.com <pedso1812@gmail.com>
2022-07-28 23:44:42 -0300

    color system all working

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,13 @@ function App() {
 	const [showGrid, setShowGrid] = useState(true);
 	const isMousePressed = useRef(false);
 	const selectedColor = useRef({ r: 255, g: 255, b: 255, a: 1 });
+	const [stateSelectedColor, setStateSelectedColor] = useState({
+		r: 255,
+		g: 255,
+		b: 255,
+		a: 1,
+	});
+	const [selectedColorCell, setSelectedColorCell] = useState(0);
 
 	return (
 		<div
@@ -37,25 +44,155 @@ function App() {
 				<RgbaColorPicker
 					className='colorPicker'
 					tabIndex={-1}
+					color={stateSelectedColor}
 					onChange={e => {
 						console.log(e);
 						selectedColor.current = e;
+						setStateSelectedColor(e);
 					}}
 				/>
 
 				<div className='colorDiv'>
-					<ColorCell />
-					<ColorCell />
-					<ColorCell />
-					<ColorCell />
-					<ColorCell />
-					<ColorCell />
-					<ColorCell />
-					<ColorCell />
-					<ColorCell />
-					<ColorCell />
-					<ColorCell />
-					<ColorCell />
+					<ColorCell
+						selectedColor={selectedColor}
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={0}
+						defaultColor={{ r: 255, g: 255, b: 255, a: 1 }}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={1}
+						defaultColor={{ r: 252, g: 244, b: 0, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={2}
+						defaultColor={{ r: 255, g: 100, b: 0, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={3}
+						defaultColor={{ r: 221, g: 2, b: 2, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={4}
+						defaultColor={{ r: 240, g: 2, b: 133, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={5}
+						defaultColor={{ r: 70, g: 0, b: 165, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={6}
+						defaultColor={{ r: 0, g: 0, b: 213, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={7}
+						defaultColor={{ r: 0, g: 174, b: 233, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={8}
+						defaultColor={{ r: 26, g: 185, b: 12, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={9}
+						defaultColor={{ r: 0, g: 100, b: 7, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={10}
+						defaultColor={{ r: 87, g: 40, b: 0, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={11}
+						defaultColor={{ r: 145, g: 113, b: 53, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={12}
+						defaultColor={{ r: 252, g: 244, b: 0, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={13}
+						defaultColor={{ r: 252, g: 244, b: 0, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={14}
+						defaultColor={{ r: 252, g: 244, b: 0, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={15}
+						defaultColor={{ r: 255, g: 255, b: 255, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={16}
+						defaultColor={{ r: 193, g: 193, b: 193, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={17}
+						defaultColor={{ r: 129, g: 129, b: 129, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={18}
+						defaultColor={{ r: 14, g: 14, b: 14, a: 1 }}
+						selectedColor={selectedColor}
+					/>
+					<ColorCell
+						stateSelectedColor={[stateSelectedColor, setStateSelectedColor]}
+						selectedColorCellState={[selectedColorCell, setSelectedColorCell]}
+						colorCellIndex={19}
+						defaultColor={{ r: 0, g: 0, b: 0, a: 1 }}
+						selectedColor={selectedColor}
+					/>
 				</div>
 			</div>
 		</div>
--- src/colorCell.jsx
+++ src/colorCell.jsx
@@ -1,3 +1,50 @@
+import { useState } from "react";
+
 export default function ColorCell(props) {
-	return <div className='colorCell'></div>;
+	const [selected, setSelected] = useState(false);
+	const [myColor, setMyColor] = useState(props.defaultColor);
+
+	if (
+		props.selectedColorCellState[0] !== props.colorCellIndex &&
+		selected === true
+	) {
+		setMyColor(props.stateSelectedColor[0]);
+		setSelected(false);
+	} else if (
+		selected === false &&
+		props.selectedColorCellState[0] === props.colorCellIndex
+	) {
+		props.stateSelectedColor[1](myColor);
+		props.selectedColor.current = myColor;
+		setSelected(true);
+	}
+
+	return selected === true ? (
+		<div
+			style={{ backgroundColor: rgbaToHex(props.stateSelectedColor[0]) }}
+			className='colorCell'>
+			<div className='selectedColorCellRing'></div>
+		</div>
+	) : (
+		<div
+			style={{ backgroundColor: rgbaToHex(myColor) }}
+			className='colorCell'
+			onMouseDown={() => {
+				props.selectedColorCellState[1](props.colorCellIndex);
+			}}></div>
+	);
+
+	function rgbaToHex({ r, g, b, a }) {
+		r = r.toString(16);
+		g = g.toString(16);
+		b = b.toString(16);
+		a = Math.round(a * 255).toString(16);
+
+		if (r.length === 1) r = "0" + r;
+		if (g.length === 1) g = "0" + g;
+		if (b.length === 1) b = "0" + b;
+		if (a.length === 1) a = "0" + a;
+
+		return "#" + r + g + b + a;
+	}
 }
